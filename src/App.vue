20d3f76524c651fc4edbee4d7a2e297f384f03e5
Muhammad Ryan Pranata <pranataryan91@gmail.com>
2024-11-24 12:23:30 +0700

    Setup Project and Implementation Page My Task

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Footer from "./components/Footer.vue";
+import Navbar from "./components/Navbar.vue";
+import TaskPages from "./pages/TaskPages.vue";
+</script>
+
+<template>
+	<Navbar />
+	<TaskPages />
+	<Footer />
+</template>
+
+<style scoped></style>
